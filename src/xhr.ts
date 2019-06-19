8386560c15021b4a2fa8685d45362044a8ac37ef
weidian <youzhijun@weidian.com>
2019-06-19 19:13:08 +0800

    简单的demo编写

--- src/xhr.ts
+++ src/xhr.ts
@@ -6,6 +6,6 @@ export default function xhr(config: AxiosRequestConfig): void {
   const { data = null, url, method = 'get' } = config
 
   const request = new XMLHttpRequest()
-  request.open(method.toUpperCase(), url!, true)
+  request.open(method.toUpperCase(), url, true)
   request.send(data)
 }
